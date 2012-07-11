d4aea6255f31facade74a26e043976d7cdd8d7da
Leo 2 <leonardo.campos@pangeatech.com.ve>
2012-07-11 16:20:24 -0430

    commit de prueba3

--- NewRichFacesApp/src/java/com/pangea/NewRichFacesApp/controllers/myBean.java
+++ NewRichFacesApp/src/java/com/pangea/NewRichFacesApp/controllers/myBean.java
@@ -11,7 +11,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
-//this is MY BEAN!! No it is not!!!
+//this is MY BEAN!! No it is not!!! yes, it is!!
 /**
  *
  * @author pangea
